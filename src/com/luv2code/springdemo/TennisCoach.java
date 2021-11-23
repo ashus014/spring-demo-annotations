8444bb58c8d0b14e0f6cb9ba8ac3c46e8d68ac84
Ashu Singh <ashu.ask.singh@gmail.com>
2021-11-23 23:19:58 +0530

    Field Injection : Using @Autowired with Spring Annotations

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
     private FortuneService fortuneService;
 
-//    @Autowired
-//    public TennisCoach(FortuneService fortuneService) {
-//        this.fortuneService = fortuneService;
-//    }
+    /*
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+     */
 
     //creating a default constructor just for logging purpose
     public TennisCoach() {
@@ -18,11 +21,14 @@ public class TennisCoach implements Coach{
     }
 
     //define a setter method
+    /*
     @Autowired
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println(">> TennisCoach : Inside setFortuneService() method");
         this.fortuneService = fortuneService;
     }
+    */
+
 
     @Override
     public String getDailyWorkout() {
